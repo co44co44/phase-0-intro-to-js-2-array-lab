c4b429672c38798a62b816aa4b6f080599e5fad5
Maria Cristina Ordonez <crisordonez44@gmail.com>
2022-09-26 15:36:27 -0500

    commit tree 939fa861b5d0c627214af151dac7d131a76e7379

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
 // Write your solution here!
+const cats =["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(name){
+    
+    return cats.push(name);
+}
+
+function destructivelyPrependCat(name){
+    
+    return cats.unshift(name);
+}
+function destructivelyRemoveLastCat(name){
+    
+    return cats.pop(name);
+}
+function destructivelyRemoveFirstCat(name){
+    
+    return cats.shift(name);
+}
+
+function appendCat(name){
+    let newCats= [...cats, name];
+    return newCats;
+}
+function prependCat(name){
+    let newCats= [name,...cats];
+    return newCats;
+}
+function removeLastCat(name){
+    return cats.slice (0,-1);
+   
+}
+function removeFirstCat(name){
+    return cats.slice (1);
+   
+}
